1575328cb5a99e330d927b0957b8d57de6083287
Ciro S <ciroalejandro1@gmail.com>
2025-04-06 11:27:11 -0400

    feat: update react details

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,18 @@ import '@fontsource-variable/space-grotesk'
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import { App } from './app'
 import queryClient from './lib/reactQuery/queryClient'
 
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
+const root = ReactDOM.createRoot(
+	document.getElementById('reactMountPoint') as HTMLElement,
+)
+root.render(
+	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<App />
-		</QueryClientProvider>,
-		document.getElementById('reactMountPoint'),
-	)
-})
+		</QueryClientProvider>
+	</React.StrictMode>,
+)
